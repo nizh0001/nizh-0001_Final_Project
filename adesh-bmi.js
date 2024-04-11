a806c5ac1cebb000d8d2938077ec1195a04b5d11
Veronikaniz <nizh0001@algonquinlive.com>
2024-04-11 11:25:47 -0400

    Complete final project

---
+++ adesh-bmi.js
@@ -0,0 +1,28 @@
+let weight = 90;
+let height = 1.91;
+let bmi = 0;
+
+function calculateBMI(weight, height) {
+  bmi = weight / Math.pow(height, 2);
+  return bmi;
+}
+
+calculateBMI(weight, height);
+
+function interpretBMI(bmi) {
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if ((bmi >= 18.5) & (bmi < 25)) {
+    return "Normal weight";
+  } else if ((bmi >= 25) & (bmi < 30)) {
+    return "Overweight";
+  } else if (bmi >= 30) {
+    return "Obese";
+  }
+}
+
+console.log(
+  `If your weight: ${weight} and height: ${height} -Your mbi is ${bmi} You are = ${interpretBMI(
+    bmi
+  )} `
+);
